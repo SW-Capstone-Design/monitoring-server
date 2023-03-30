19f043ff6bb351dd1afcc8c179e2aafeccee283e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-03-30 22:09:36 +0900

    Feat : Mapping User Table

--- src/main/java/kr/co/monitoringserver/persistence/BaseEntity.java
+++ src/main/java/kr/co/monitoringserver/persistence/BaseEntity.java
@@ -1,4 +1,29 @@
 package kr.co.monitoringserver.persistence;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(callSuper = false)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    protected LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    protected LocalDate modifiedAt;
 }
--- src/main/java/kr/co/monitoringserver/persistence/entity/User.java
+++ src/main/java/kr/co/monitoringserver/persistence/entity/User.java
@@ -1,13 +1,21 @@
 package kr.co.monitoringserver.persistence.entity;
 
 import jakarta.persistence.*;
+import kr.co.monitoringserver.persistence.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tbl_users")
-public class User {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+@AttributeOverride(     // 하나의 Entity에서 같은 값 타입을 사용하면 Column 명이 중복되므로 Column 명 속성을 재정의
+        name = "id",
+        column = @Column(name = "users_id", length = 4))
+public class User extends BaseEntity {
 
     private String identity;
 
@@ -16,4 +24,6 @@ public class User {
     private String name;
 
     private String phone;
+
+    private String department;
 }
