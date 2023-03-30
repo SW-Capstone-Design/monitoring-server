d0e98d2a04a200e3eed0371f307e9190ecc5249f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-03-30 19:50:28 +0900

    init

---
+++ src/test/java/kr/co/monitoringserver/MonitoringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.monitoringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
