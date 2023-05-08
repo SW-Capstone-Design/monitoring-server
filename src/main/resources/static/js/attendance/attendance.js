0d10288257197c430974c9ff9600e69e7704c912
brobro332 <brobro332@github.com>
2023-05-09 03:18:25 +0900

    Feat : Implement Attend Delete Publishing

--- src/main/resources/static/js/attendance/attendance.js
+++ src/main/resources/static/js/attendance/attendance.js
@@ -6,6 +6,9 @@ let index = {
             $("#btn-update").on("click", ()=>{
                 this.update();
             });
+            $("#btn-del").on("click", ()=>{
+                this.del();
+            });
 		},
 
 		register: function() {
@@ -39,7 +42,7 @@ let index = {
 		},
 
 		update: function() {
-		        param1 = $("#identity").val();
+		        var param = $("#identity").val();
 
                 let data = {
                         enterTime: $("#enterTime").val(),
@@ -49,7 +52,7 @@ let index = {
 
                 $.ajax({
                     type: "PUT",
-                    url: "/attendance/"+param1,
+                    url: "/attendance/"+param,
                     data: JSON.stringify(data),
                     contentType: "application/json; charset=utf-8",
                     dataType: "json"
@@ -64,7 +67,36 @@ let index = {
                 }).fail(function(error) {
                     alert(JSON.stringify(error));
                 });
-            }
+            },
+
+        		del: function() {
+                        if (confirm("삭제를 진행하시겠습니까?")) {
+                        var param = $("#identity").val();
+                        var date = $("#date").val();
+
+                        let data = {
+                                date: $("#date").val()
+                        };
+
+                        $.ajax({
+                            type: "DELETE",
+                            url: "/attendance/"+param+"?date="+date,
+                            data: JSON.stringify(data),
+                            contentType: "application/json; charset=utf-8",
+                            dataType: "json"
+                        }).done(function(resp) {
+                            if(resp.status == 400 || resp.status == 500){
+                                  alert("출결정보 삭제에 실패하였습니다.");
+                              }
+                              else{
+                                  alert("출결정보 삭제가 완료되었습니다.");
+                                  location.href = "/admin/attendance/list";
+                            }
+                        }).fail(function(error) {
+                            alert(JSON.stringify(error));
+                        });
+                    }
+                }
 }
 
 index.init();
