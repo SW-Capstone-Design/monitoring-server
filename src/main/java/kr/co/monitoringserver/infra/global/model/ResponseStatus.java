2fcd21a18a972c5ee705d3aad070267b213b48d7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-06-01 16:40:16 +0900

    Feat : Add ResponseStatus ErrorCode

--- src/main/java/kr/co/monitoringserver/infra/global/model/ResponseStatus.java
+++ src/main/java/kr/co/monitoringserver/infra/global/model/ResponseStatus.java
@@ -8,20 +8,27 @@ public enum ResponseStatus {
 
     // Success
     SUCCESS_EXECUTE(HttpStatus.OK, "성공적으로 실행되었습니다"),
+
     SUCCESS_CREATED(HttpStatus.CREATED, "성공적으로 실행되었고, 새 리소스가 생성되었습니다"),
 
-    // Fail
-    FAIL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "실행에 실패했고, 해당 요청이 잘못되었습니다"),
+    SUCCESS_ACCEPTED(HttpStatus.ACCEPTED, "성공적으로 실행되었지만, 요청 처리에 대한 결과가 명확하지 않습니다"),
 
-    // TODO : 오류 종류 추가 및 수정
+    // Fail
+    FAIL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "실행에 실패했고, 잘못된 문법으로 인해 요청을 이해할 수 없습니다"),
 
     FAIL_INVALID_VALUE(HttpStatus.BAD_REQUEST, "실행에 실패했고, 해당 항목의 값이 존재하지 않습니다"),
+
     FAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "실행에 실패했고, 해당 정보를 찾을 수 없습니다"),
-    FAIL_NOT_AUTHENTICATE(HttpStatus.UNAUTHORIZED, "실행에 실패했고, 해당 권한이 없습니다"),
+
+    FAIL_NOT_AUTHENTICATE(HttpStatus.UNAUTHORIZED, "실행에 실패했고, 해당 사용자는 콘텐츠 접근을 위한 인증이 필요합니다"),
+
+    FAIL_FORBIDDEN(HttpStatus.FORBIDDEN, "실행에 실패했고, 해당 사용자는 콘텐츠에 접근할 권리를 가지고 있지 않습니다"),
+
     FAIL_INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "실행에 실패했고, 해당 항목의 값이 존재하지 않습니다"),
 
     // User
     DUPLICATE_USER_ATTENDANCE(HttpStatus.BAD_REQUEST, "해당 사용자는 이미 출석하였습니다"),
+
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 사용자 정보를 찾지 못하였습니다"),
 
     // UserAttendance
